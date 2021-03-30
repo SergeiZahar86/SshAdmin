ac93aff575763f6b6a234d100be456a2150a49a2
Сергей Захаров <sergei86zahar@mail.ru>
2021-03-30 16:23:02 +0700

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IncubeAdmin
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown(); // выход из программы
+            Environment.Exit(0);
+        }
+        private void ExpandButton_Click(object sender, RoutedEventArgs e) // сворачивание окна
+        {
+            if (WindowState == WindowState.Normal)
+            {
+                this.WindowState = WindowState.Maximized;
+                this.Padding = new Thickness(100);
+                this.Margin = new Thickness(100);
+                HorizontalAlignment = HorizontalAlignment.Center;
+
+            } else
+            {
+                WindowState = WindowState.Normal;
+            }
+        }
+
+        private void MinButton_Click(object sender, RoutedEventArgs e) // сворачивание окна
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+    }
+}
