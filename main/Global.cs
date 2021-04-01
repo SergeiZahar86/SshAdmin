645b6ed2e91e9e633d98d54d2af43a0769af6132
Сергей Захаров <sergei86zahar@mail.ru>
2021-04-01 17:20:35 +0700

    1

---
+++ main/Global.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IncubeAdmin
+{
+    class Global
+    {
+        private static Global instance;
+        private User User;
+        private string connectionString;
+
+        public static Global getInstance() // возвращает singleton объекта Global
+        {
+            if (instance == null)
+                instance = new Global();
+            return instance;
+        }
+
+        private Global()
+        {
+            var appSettings = ConfigurationManager.AppSettings;
+            connectionString = appSettings["connectionString"];
+
+
+            using (var connection = new SQLiteConnection("Data Source=usersdata.db"))
+            {
+                connection.Open();
+            }
+            //Console.Read();
+
+        }
+
+    }
+}
---
+++ main/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IncubeAdmin
+{
+    class User
+    {
+        private int Id { get; set; }
+        private string Name { get; set; }
+        private string Pass { get; set; }
+
+
+        public User(int id, string name, string pass)
+        {
+            Id = id;
+            Name = name;
+            Pass = pass;
+        }
+        public User() { }
+    }
+}
